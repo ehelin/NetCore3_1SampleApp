06a8cf6cfaf912fb693a1dc6c8ec2d0e714e7fdf
Eric Helin <erichelinse@gmail.com>
2021-01-24 15:58:03 -0600

    running commands individually...docker ones still not firing.

--- NetCore3_1SampleApp/AwsCodePipeline/EC2RunCommands/index.js
+++ NetCore3_1SampleApp/AwsCodePipeline/EC2RunCommands/index.js
@@ -1,33 +1,66 @@
 var SSH = require('simple-ssh');
-var Alexa = require("alexa-sdk");
 var fs = require('fs');
 var AWS = require('aws-sdk');
 
 AWS.config.update({ region: "us-east-2" });
 
 var ssh = new SSH({
-	host: 'host w/o protocol here',
+	host: 'your host w/o protocol here!',
 	user: 'ec2-user',
-	key: fs.readFileSync('pem file here')
+	key: fs.readFileSync('NetCore3_1KeyPairV2.pem'),
+	timeout: 120000
 });
 
-exports.handler = function(event, context, callback) {
-	var alexa = Alexa.handler(event, context);
-	alexa.registerHandlers(handlers);
+async function RunCommand(command) {
+	console.log('Start RunCommand(command) - command: ' + command);	
 
-	ssh.on("close", function () {alexa.execute()});
-	ssh.exec('touch DockerFile', { 
-	out: console.log.bind(console)
-	})
-	.exec('exit', {
-	out: console.log.bind(console)
-	}).start();
-	};
+	return new Promise(function(resolve, reject) {  
+	  let ourout = "";
+  
+	  ssh.exec(command, {
+		exit: function() {
+		  ourout += "\nsuccessfully exited!";
+		  resolve(ourout);
+		},
+		out: function(stdout) {
+		  ourout += stdout;
+		}
+	  }).start({
+		success: function() {
+		  console.log("successful connection! command - " + command);
+		},
+		fail: function(e) {
+		  console.log("failed connection! command - " + command);
+		  console.log(e);
+		}
+	  });  
+	});
+}
 
-	var handlers = {
-	'LaunchRequest': function(){
-	this.emit(':tell', 'Now opening the garage door.');
+exports.handler = async function(event, context, callback) {
+	console.log('Start exports.handler()');	
+  
+	let commands = [
+		'sudo yum update -y',
+		'sudo yum install git -y',
+		'sudo amazon-linux-extras install docker -y',
+		'sudo service docker start',
+		'cd home/ec2-user',
+		'git clone https://github.com/ehelin/NetCore3_1SampleApp.git',
+		'cd NetCore3_1SampleApp',
+		'sudo docker build . -t myawesomerepository',
+		'sudo docker run -it -p 80:80 myawesomerepository'
+	];
+	let res = null;
+	for(let i=0; i<commands.length;i++){
+		res = await RunCommand(commands[i]);
 	}
-};
-//console.log('hi mom!');
 
+	console.log('exports.handler() done');
+  
+	const response = {
+	  statusCode: 200,
+	  body: res,
+	};
+	return response;
+};
