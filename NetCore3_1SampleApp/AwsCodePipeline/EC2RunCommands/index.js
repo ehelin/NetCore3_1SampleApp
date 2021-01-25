003df2c0fa5ca4e82f0508f1ef5522e74b92435a
Eric Helin <erichelinse@gmail.com>
2021-01-25 12:26:30 -0600

    moved ssh object into method

--- NetCore3_1SampleApp/AwsCodePipeline/EC2RunCommands/index.js
+++ NetCore3_1SampleApp/AwsCodePipeline/EC2RunCommands/index.js
@@ -1,64 +1,55 @@
 var SSH = require('simple-ssh');
 var fs = require('fs');
-var AWS = require('aws-sdk');
 
-AWS.config.update({ region: "us-east-2" });
+async function RunCommand(command) {	
+	var ssh = new SSH({
+		host: 'your host w/o protocol here!',
+		user: 'ec2-user',
+		key: fs.readFileSync('NetCore3_1KeyPairV2.pem'),
+		timeout: 120000
+	});
 
-var ssh = new SSH({
-	host: 'your host w/o protocol here!',
-	user: 'ec2-user',
-	key: fs.readFileSync('NetCore3_1KeyPairV2.pem'),
-	timeout: 120000
-});
-
-async function RunCommand(command) {
 	return new Promise(function(resolve, reject) {  
-	  let ourout = "";
+	  let output = "";
   
 	  ssh.exec(command, {
 		exit: function() {
-		  ourout += "\nsuccessfully exited!";
-		  resolve(ourout);
+			output += "\nsuccessfully exited!";
+		  resolve(output);
 		},
 		out: function(stdout) {
 		  console.log('output - ' + stdout);
 		}
 	  }).start({
 		success: function() {
-		  console.log('success output - ' + ourout);
+		  console.log('success output - ' + output);
 		},		
-        	out: function() {
-		  console.log.bind(console);
+        out: function() {
+			console.log.bind(console);
 		},
 		fail: function(e) {
 		  console.log('Fail - ' + e);
 		},
 		err: function(stderr) {
-		  console.log('Error - ' + stderr); 
+			console.log('Error - ' + stderr); 
 		},
 		exit: function(code) {
-		  console.log('Exit code ' + code); 
+			console.log('Exit code ' + code); 
 		}
 	  });  
 	});
 }
 
 exports.handler = async function(event, context, callback) {
-	console.log('Start exports.handler()');	
+	console.log('exports.handler() starting...');	
   
 	let commands = [
-		//1
 		'sudo yum update -y',
 		'sudo yum install git -y',
 		'sudo amazon-linux-extras install docker -y',
 		'sudo service docker start',
 		'git clone https://github.com/ehelin/NetCore3_1SampleApp.git',
-
-		//2
-		'cd NetCore3_1SampleApp; sudo docker build . -t myawesomerepository',
-		
-		//3
-		'cd NetCore3_1SampleApp; sudo docker run -it -p 80:80 myawesomerepository',
+		'cd NetCore3_1SampleApp; sudo docker build . -t myawesomerepository'
 	];
 
 	let res = null;
