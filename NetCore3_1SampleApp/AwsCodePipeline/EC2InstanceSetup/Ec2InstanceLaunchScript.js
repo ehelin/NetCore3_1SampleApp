c79ef08485851263fb8c0fe71af221257b951f03
Eric Helin <erichelinse@gmail.com>
2021-01-26 21:46:43 -0600

    passing created instance's ip back

--- NetCore3_1SampleApp/AwsCodePipeline/EC2InstanceSetup/Ec2InstanceLaunchScript.js
+++ NetCore3_1SampleApp/AwsCodePipeline/EC2InstanceSetup/Ec2InstanceLaunchScript.js
@@ -5,32 +5,28 @@ exports.handler = async (event) => {
     console.log("Starting EC2 creation...");
     
     console.log(JSON.stringify(event, null, 2))
-	var jobId = event["CodePipeline.job"].id
-	var params = {
-		jobId: jobId
-	}
+    var jobId = event["CodePipeline.job"].id
 
-    console.log("Load credentials and set region from JSON file");
+    console.log("Set region...");
     AWS.config.update({region: 'us-east-2'});
 
-	var userData = '#!/bin/bash'
-					+ 'yum update -y'
-					+ 'yum install git -y'
-					+ 'amazon-linux-extras install docker -y'
-					+ 'service docker start'
-					+ 'cd home/ec2-user'
-					+ 'git clone https://github.com/ehelin/NetCore3_1SampleApp.git'
-					+ 'cd NetCore3_1SampleApp'
-					+ 'docker build . -t myawesomerepository'    
-	var userDataEncoded = new Buffer(userData).toString('base64');
-
-    console.log("AMI is amzn-ami-2011.09.1.x86_64-ebs");
+    console.log("Setup instance params...");
+    var userData = `#!/bin/bash
+		    yum update -y
+		    yum install git -y
+		    amazon-linux-extras install docker -y
+		    service docker start
+	       	    cd home/ec2-user
+		    git clone https://github.com/ehelin/NetCore3_1SampleApp.git
+		    cd NetCore3_1SampleApp
+		    docker build . -t myawesomerepository`   
+    var userDataEncoded = new Buffer(userData).toString('base64');
     var instanceParams = {
        ImageId: 'ami-09558250a3419e7d0',
        InstanceType: 't2.micro',
-       KeyName: 'NetCore3_1KeyPairV2',
+       KeyName: 'NetCore3_1KeyPairV2',					// key setup in aws 
        UserData: userDataEncoded,
-	   SecurityGroupIds: ['sg-0f2d4961192b272f3'],
+       SecurityGroupIds: ['sg-0f2d4961192b272f3'],		// security group setup in aws
        MinCount: 1,
        MaxCount: 1
     };
@@ -38,21 +34,28 @@ exports.handler = async (event) => {
     console.log("Create a promise on an EC2 service object");
     var instancePromise = new AWS.EC2({apiVersion: '2016-11-15'}).runInstances(instanceParams).promise();
     
-    console.log("Handle promise's fulfilled/rejected states");
-    
+    console.log("Handle promise's fulfilled/rejected states");    
     return instancePromise.then(
       function(data) {
-    	console.log("inside then");
         console.log(data);
         var instanceId = data.Instances[0].InstanceId;
         console.log("Created instance", instanceId);
-			
-		return codepipeline.putJobSuccessResult(params).promise()
+
+	var ipAddress = data.Instances[0].ipAddress;
+        console.log("Created instance ipAddress", ipAddress);
+
+	var params = {
+		jobId: jobId,
+	    outputVariables: {
+			ipAddress: ipAddress,
+	    } 
+	}
+
+	return codepipeline.putJobSuccessResult(params).promise()
     }).catch(
         function(err) {
-    	console.log("inside error");
         console.error(err, err.stack);
         
-		return codepipeline.putJobFailureResult(params).promise()
+	return codepipeline.putJobFailureResult(params).promise()
     });
 }
