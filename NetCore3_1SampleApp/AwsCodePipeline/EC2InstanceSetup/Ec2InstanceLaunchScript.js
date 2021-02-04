cfdac9f66296b7817dcb57dfcc3d892f12f8022b
Eric Helin <erichelinse@gmail.com>
2021-02-03 22:19:30 -0600

    Attempting to get public ip from instance...was not present

--- NetCore3_1SampleApp/AwsCodePipeline/EC2InstanceSetup/Ec2InstanceLaunchScript.js
+++ NetCore3_1SampleApp/AwsCodePipeline/EC2InstanceSetup/Ec2InstanceLaunchScript.js
@@ -1,5 +1,7 @@
 var AWS = require('aws-sdk')
 var codepipeline = new AWS.CodePipeline()
+const { promisify } = require('util');
+const sleep = promisify(setTimeout);
 
 exports.handler = async (event) => {
     console.log("Starting EC2 creation...");
@@ -12,24 +14,31 @@ exports.handler = async (event) => {
 
     console.log("Setup instance params...");
     var userData = `#!/bin/bash
-		    yum update -y
-		    yum install git -y
-		    amazon-linux-extras install docker -y
-		    service docker start
-	       	    cd home/ec2-user
-		    git clone https://github.com/ehelin/NetCore3_1SampleApp.git
-		    cd NetCore3_1SampleApp
-		    docker build . -t myawesomerepository`   
+				yum update -y
+				yum install git -y
+				amazon-linux-extras install docker -y
+				service docker start
+				cd home/ec2-user
+				git clone https://github.com/ehelin/NetCore3_1SampleApp.git
+				cd NetCore3_1SampleApp
+				docker build . -t myawesomerepository`   
     var userDataEncoded = new Buffer(userData).toString('base64');
     var instanceParams = {
        ImageId: 'ami-09558250a3419e7d0',
        InstanceType: 't2.micro',
        KeyName: 'NetCore3_1KeyPairV2',					// key setup in aws 
        UserData: userDataEncoded,
-       SecurityGroupIds: ['sg-0f2d4961192b272f3'],		// security group setup in aws
+	   SecurityGroupIds: ['sg-0f2d4961192b272f3'],		// security group setup in aws
        MinCount: 1,
        MaxCount: 1
     };
+    
+     var params = {
+	jobId: jobId,
+	//outputVariables: {
+	//	ipAddress: ipAddress,
+	//} 
+     };
     
     console.log("Create a promise on an EC2 service object");
     var instancePromise = new AWS.EC2({apiVersion: '2016-11-15'}).runInstances(instanceParams).promise();
@@ -41,17 +50,26 @@ exports.handler = async (event) => {
         var instanceId = data.Instances[0].InstanceId;
         console.log("Created instance", instanceId);
 
-	var ipAddress = data.Instances[0].ipAddress;
-        console.log("Created instance ipAddress", ipAddress);
+	console.log("Create for describe_instances()");
+	var describeInstanceParams = { InstanceIds: [instanceId] };
+	var instanceDescribePromise = new AWS.EC2({apiVersion: '2016-11-15'}).describeInstances(describeInstanceParams).promise();
 
-	var params = {
-		jobId: jobId,
-	    outputVariables: {
-			ipAddress: ipAddress,
-	    } 
-	}
+	return instanceDescribePromise.then(
+		  function(data) {
+			console.log("Call back of describe_instances()");
+			console.log(JSON.stringify(data, null, 2));
 
-	return codepipeline.putJobSuccessResult(params).promise()
+			//var ipAddress = data.Instances[0].ipAddress;
+			//console.log("Created instance ipAddress", ipAddress);	
+
+			return codepipeline.putJobSuccessResult(params).promise()
+		}).catch(
+			function(err) {
+			console.error(err, err.stack);
+
+			return codepipeline.putJobFailureResult(params).promise()
+		})
+	);
     }).catch(
         function(err) {
         console.error(err, err.stack);
